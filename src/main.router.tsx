6c2041acbad10adb5e8dd924d781daa99ba27967
Dzianis Kavaliou <ds.kavaliou@gmail.com>
2024-06-14 16:10:07 +0300

    ref:replace-lazy-imports

--- src/main.router.tsx
+++ src/main.router.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import RootLayout from "./pages/root.layout";
+import RootPage from "./pages/root.page";
 
-const RootLayout = React.lazy(() => import("./pages/root.layout"));
-const RootPage = React.lazy(() => import("./pages/root.page"));
+// const RootLayout = React.lazy(() => import("./pages/root.layout"));
+// const RootPage = React.lazy(() => import("./pages/root.page"));
 
 const router = createBrowserRouter([
   { element: <RootLayout />, children: [{ path: "", element: <RootPage /> }] },
