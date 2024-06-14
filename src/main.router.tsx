23083471a386a970198ccccb47056c3f877a4c50
Dzianis Kavaliou <ds.kavaliou@gmail.com>
2024-06-14 20:00:23 +0300

    upd:add-basename

--- src/main.router.tsx
+++ src/main.router.tsx
@@ -4,13 +4,18 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 const RootLayout = lazy(() => import("./pages/root.layout"));
 const RootPage = lazy(() => import("./pages/root.page"));
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "",
+      element: <RootLayout />,
+      children: [{ path: "", element: <RootPage /> }],
+    },
+  ],
   {
-    path: "",
-    element: <RootLayout />,
-    children: [{ path: "", element: <RootPage /> }],
-  },
-]);
+    basename: "/CV/",
+  }
+);
 
 export default function AppRouterProvider() {
   return <RouterProvider router={router} />;
