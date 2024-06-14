31da2248571fb9cf06cc8a160c65b538afffa24c
Dzianis Kavaliou <ds.kavaliou@gmail.com>
2024-06-14 16:03:33 +0300

    upd:base-url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const isProduction = command === "build";
+  return {
+    plugins: [react()],
+    base: isProduction ? "/CV/" : "/",
+  };
 });
