1137262e78067325baa19484a72160ed7fcd6a4d
Dzianis Kavaliou <ds.kavaliou@gmail.com>
2024-07-22 20:55:39 +0300

    update:footer-markup

--- src/pages/root.layout.tsx
+++ src/pages/root.layout.tsx
@@ -14,12 +14,12 @@ export default function RootLayout() {
               alt="Dzianis Kavaliou"
             />
             <h1 className="font-medium text-3xl">
-              Dzianis <span className="text-blue-500">Kavaliou</span>
+              Dzianis <span className="text-blue-600">Kavaliou</span>
             </h1>
             <ul className="flex flex-wrap justify-center w-4/5 gap-x-4 gap-y-2 text-lg text-center">
               {details.map(({ name, value, type }) => (
                 <li key={name}>
-                  <span className="text-blue-500 font-medium">{name}:</span>
+                  <span className="text-blue-600 font-medium">{name}:</span>
                   &nbsp;
                   {type === "email" && <a href={`mailto:${value}`}>{value}</a>}
                   {type === "tel" && <a href={`tel:${value}`}>{value}</a>}
@@ -38,8 +38,20 @@ export default function RootLayout() {
       <main className="py-4">
         <Outlet />
       </main>
-      <footer className="py-4 text-center shadow-inner">
-        {new Date().getFullYear()}
+      <footer className="shadow-inner">
+        <div className="container text-center py-5 text-sm">
+          <p>
+            <span>Designed and developed by</span>
+            &nbsp;
+            <a
+              href="https://github.com/ds-kavaliou"
+              className="tracking-wide font-medium uppercase text-xs"
+            >
+              Dzianis Kavaliou
+            </a>
+          </p>
+          <p>{new Date().getFullYear()}</p>
+        </div>
       </footer>
     </>
   );
